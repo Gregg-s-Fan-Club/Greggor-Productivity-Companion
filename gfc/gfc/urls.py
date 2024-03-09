c0beb42a38c9ba8b5c78e88fed65bcd3ede3be4b
bhavik gilbert <83555685+Bhavik-Gilbert@users.noreply.github.com>
2024-03-09 14:46:58 +0000

    config: media folder

--- gfc/gfc/urls.py
+++ gfc/gfc/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, URLPattern
+from django.conf import settings
+from django.conf.urls.static import static
 from greggor_productivity_companion import views
 
 urlpatterns = [
@@ -26,3 +28,6 @@ urlpatterns = [
     ),
     path('dashboard/', views.dashboard_view, name='dashboard')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
